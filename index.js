5307253e35e91f95fc5b5530841de82e9df55559
Mounir Benabderrahmane <bnmounir7@gmail.com>
2020-02-18 11:10:22 -0500

    fix mongoose deprecation warning

--- index.js
+++ index.js
@@ -9,17 +9,20 @@ require('./models/User');
 require('./services/passport');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(keys.mongoURI);
+mongoose.connect(keys.mongoURI, {
+    useUnifiedTopology: true,
+    seNewUrlParser: true
+});
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(
-  cookieSession({
-    maxAge: 30 * 24 * 60 * 60 * 1000,
-    keys: [keys.cookieKey]
-  })
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
 );
 app.use(passport.initialize());
 app.use(passport.session());
@@ -27,22 +30,22 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/userRoutes')(app);
 app.get('/', (req, res) => {
-  let adminContent = `
+    let adminContent = `
     <div>
       You don't appear to be logged in.  You can log in by visiting
       <a href="/auth/google">the Authentication Route</a>. You could
       also look at details about yourself at <a href="/current_user">the Current User route</a>
     </div>
   `;
-  if (req.user) {
-    adminContent = `
+    if (req.user) {
+        adminContent = `
       <div>
         You appear to be logged in, so you can visit <a href="/admins">the Admins route</a>
         or you can <a href="/logout">Logout</a>.
       </div>
     `;
-  }
-  res.send(`
+    }
+    res.send(`
     <div>
       <h4>Hi!  Welcome to the React SSR API</h4>
       <div>
